62119fc1df537d7c79067f23b0360ae4fcc9e815
Junior <cleveranalista@gmail.com>
2021-02-15 19:06:50 -0300

    Startin to implement Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,14 +1,24 @@
 package application;
 
+import java.util.Scanner;
+
 import boardgame.Position;
 
 public class Program {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 		
-		Position pos = new Position(3, 5);
+		System.out.println("Digie a Linha");
+		int row = sc.nextInt();
+		System.out.println("Digie a Coluna");
+		int column = sc.nextInt();
+		
+		Position pos = new Position(row, column);
 		System.out.println(pos);
 
+		
+		sc.close();
 	}
 
 }
